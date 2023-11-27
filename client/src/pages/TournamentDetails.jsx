b4beb868f0d6a4eec5b7710b15570621783a4844
xdbx114 <Arturk8791@hotmail.com>
2023-11-27 18:59:37 +0100

    Refactor CSS styles and add navigation links to TournamentDetails page

--- client/src/pages/TournamentDetails.jsx
+++ client/src/pages/TournamentDetails.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Functions from '../components/Functions';
-import { useParams, Link } from 'react-router-dom'; // import useParams
+import { useParams, Link, NavLink, Routes, Route } from 'react-router-dom';
 import AuthUser from '../components/AuthUser';
-import { SingleEliminationBracket, DoubleEliminationBracket, Match, MATCH_STATES, SVGViewer, createTheme } from '@g-loot/react-tournament-brackets';
+import { SingleEliminationBracket, DoubleEliminationBracket, Match, MATCH_STATES, SVGViewer, createTheme, Bracket } from '@g-loot/react-tournament-brackets';
 
 export const TournamentDetails = () => {
   const { getGameName, getStatusName } = Functions();
@@ -17,6 +17,13 @@ export const TournamentDetails = () => {
   const { id } = useParams(); // używamy hooka useParams
   const user = getUser();
   const isUserParticipant = participants.some(participant => participant.UserID === user.id);
+
+  const activeLinkClass = 'text-white border-b-4 border-orange-500';
+  const inactiveLinkClass = 'text-gray-300 hover:text-orange-500 hover:border-b-2 hover:border-orange-500';
+  const [activeTab, setActiveTab] = useState("summary");
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
 
   const addParticipant = async () => {
     try {
@@ -104,15 +111,10 @@ export const TournamentDetails = () => {
     fetchMatches();
   }, [id]); // aktualizujemy tablicę zależności, aby używała id
 
-  if (!tournament || loadingMatches) {
-    return <div className='container'><div className='box'>Ładowanie...</div></div>;
-  }
-
-  const isUserOrganizer = tournament.user_id == user.id;
-  return (
-    <div className='container'>
-        <div className='box'>
-        <h2>{tournament.TournamentName}</h2>
+  
+  const Summary = () => {
+    return (
+      <div className="participants"><h2>{tournament.TournamentName}</h2>
         <p>Organizator: {tournament.organizer}</p>
         <p>Format: {tournament.TournamentFormat}</p>
         <p>Prywatność: {tournament.Privacy}</p>
@@ -121,43 +123,119 @@ export const TournamentDetails = () => {
         <p>Pula nagród: {tournament.Prizepool}</p>
         {isUserParticipant ? <button onClick={deleteParticipant(user.id)}>Zrezygnuj z udziału</button> : <button onClick={addParticipant}>Dołącz jako uczestnik</button>}
         {isUserOrganizer ? <><button className='organizer' onClick={generateMatches(tournament.TournamentID)}>Generuj</button><button className='organizer'>Usuń</button><Link to='/tournamentEdit' ><button className='organizer'>Edytuj</button></Link></> : null}
-        <div className='participants'>
-        <h2>Uczestnicy:</h2>
+        </div>
+    );
+  };
+
+  const Participants = () => {
+    return (
+      <div className="participants"><h2>Uczestnicy:</h2>
         <ul>
           {participants.map(participant => (
             <li key={participant.UserID}>{participant.UserID} - {participant.nickname} {isUserOrganizer ? <>- <a href="#" onClick={deleteParticipant(participant.UserID)}>Usuń</a></> : null}</li> // Zakładam, że uczestnik ma pola id i name
           ))}
-        </ul>
-        <h2>Mecze:</h2>
-        {!loadingMatches && matchesdata.length > 0 && (
-        <SingleEliminationBracket
-          matches={matchesdata}
-          matchComponent={Match}
-          svgWrapper={({ children, ...props }) => (
-            <SVGViewer width={1300} height={1300} {...props} background="#0f0f0f" SVGBackground="#0f0f0f">
-              {children}
-            </SVGViewer>
-          )}
-        />
-        )}
-        {isUserOrganizer ? <>
-        <h2>Zarządzaj meczami:</h2>
-          {matchesdataorganizer.map(match => (
-            <div key={match.id} className='matchlist'>
-            <p>{match.id} - {match.nick1} vs {match.nick2} - {match.winner_id}</p>
-            <div className='matchscore'>
-              <label>{match.nick1}</label>
-              <input type="number" placeholder="0" id={`score1-${match.id}`} />
-            </div>
-            <div className='matchscore'>
-              <label>{match.nick2}</label>
-              <input type="number" placeholder="0" id={`score2-${match.id}`} />
-            </div>
-            <button onClick={() => updateMatchScore(match.id, document.getElementById(`score1-${match.id}`).value, document.getElementById(`score2-${match.id}`).value)}>Aktualizuj</button>
+        </ul></div>
+    );
+  };
+
+ 
+  const Matches = () => {
+    return (<div className="participants">
+    <h2>Mecze:</h2>
+      {isUserOrganizer ? <>
+      <h2>Zarządzaj meczami:</h2>
+        {matchesdataorganizer.map(match => (
+          <div key={match.id} className='matchlist'>
+          <p>{match.id} - {match.nick1} vs {match.nick2} - {match.winner_id}</p>
+          <div className='matchscore'>
+            <label>{match.nick1}</label>
+            <input type="number" placeholder="0" id={`score1-${match.id}`} />
           </div>
-            ))}
-        </> : null}
+          <div className='matchscore'>
+            <label>{match.nick2}</label>
+            <input type="number" placeholder="0" id={`score2-${match.id}`} />
+          </div>
+          <button onClick={() => updateMatchScore(match.id, document.getElementById(`score1-${match.id}`).value, document.getElementById(`score2-${match.id}`).value)}>Aktualizuj</button>
         </div>
+          ))}
+    </> : null}
+    </div>);
+  };
+
+  
+const Brackett = () => {
+  return (<div className="participants">
+  {!loadingMatches && matchesdata.length > 0 && (
+    <SingleEliminationBracket
+      matches={matchesdata}
+      matchComponent={Match}
+      svgWrapper={({ children, ...props }) => (
+        <SVGViewer width={1300} height={1300} {...props} background="#0f0f0f" SVGBackground="#0f0f0f">
+          {children}
+        </SVGViewer>
+      )}
+    />
+  )}</div>
+  );
+};
+
+  if (!tournament || loadingMatches) {
+    return <div className='container'><div className='box'>Ładowanie...</div></div>;
+  }
+
+  const isUserOrganizer = tournament.user_id == user.id;
+
+  return (
+    <div className='container'>
+        <div className='box'>
+        <div className='navbar border-b border-orange-800 navbar-shadow'>
+          <ul className='flex justify-start p-3 space-x-4'>
+            <li>
+              <NavLink to={``} className={activeTab === "summary" ? activeLinkClass : inactiveLinkClass} onClick={() => handleTabChange("summary")}>
+                Podsumowanie
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={``} className={activeTab === "bracket" ? activeLinkClass : inactiveLinkClass} onClick={() => handleTabChange("bracket")}>
+                Drabinka
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={``} className={activeTab === "participants" ? activeLinkClass : inactiveLinkClass} onClick={() => handleTabChange("participants")} >
+                Uczestnicy
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={``} className={activeTab === "matches" ? activeLinkClass : inactiveLinkClass} onClick={() => handleTabChange("matches")} >
+                Mecze
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={``} className={activeTab === "classification" ? activeLinkClass : inactiveLinkClass} onClick={() => handleTabChange("classification")}>
+                Klasyfikacja
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={``} className={activeTab === "rules" ? activeLinkClass : inactiveLinkClass} onClick={() => handleTabChange("rules")}>
+                Zasady
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={``} className={activeTab === "admin" ? activeLinkClass : inactiveLinkClass} onClick={() => handleTabChange("admin")}>
+                Admin
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={``} className={activeTab === "settings" ? activeLinkClass : inactiveLinkClass} onClick={() => handleTabChange("settings")} >
+                Ustawienia
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        {activeTab === "summary" && <Summary />}
+        {activeTab === "bracket" && <Brackett />}
+        {activeTab === "participants" && <Participants />}
+        {activeTab === "matches" && <Matches />}
         </div>
     </div>
   );
