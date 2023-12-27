160d9b841b9971e475ca69575860fc9516ba4a27
Tox1cMaster <adriand4413@gmail.com>
2023-12-27 23:19:45 +0100

    create tournament form visual changes

--- client/src/pages/CreateTournament.jsx
+++ client/src/pages/CreateTournament.jsx
@@ -55,63 +55,69 @@ export const CreateTournament = () => {
     return (
         <div className='container'>
             <div className='box py-4'>
-                <h2 className='my-4'>Utwórz turniej</h2>
-                <form onSubmit={handleSubmit}>
-                    <div className='py-2'>
-                        <label htmlFor="TournamentName">Nazwa turnieju:</label>
-                        <input
-                            type="text"
-                            name="TournamentName"
-                            value={formData.TournamentName}
-                            onChange={handleInputChange}
-                            placeholder="Nazwa turnieju"
-                            required
-                        />
+                <h2 className='my-4 xs:text-center'>Utwórz turniej</h2>
+                <form className='w-full' onSubmit={handleSubmit}>
+                    <div className="flex md:flex-row justify-center items-center xs:flex-col xs:w-full md:w-6/12">
+                        <div className='md:mr-5 xs:mr-0 md:w-6/12 xs:w-full'>
+                            <label htmlFor="TournamentName">Nazwa turnieju:</label>
+                            <input
+                                type="text"
+                                name="TournamentName"
+                                value={formData.TournamentName}
+                                onChange={handleInputChange}
+                                placeholder="Nazwa turnieju"
+                                required
+                            />
+                        </div>
+                        <div className='py-2 md:w-6/12 xs:w-full'>
+                            <label htmlFor="GameID">Game:</label>
+                            <select name="GameID" onChange={handleInputChange} value={formData.GameID} required>
+                                <option value="">Wybierz grę</option>
+                                {games.map((game) => (
+                                    <option key={game.GameID} value={game.GameID}>
+                                        {game.GameName}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                        </div>
+                        <div className="flex md:flex-row justify-center items-center xs:flex-col xs:w-full md:w-6/12">
+                        <div className='md:mr-5 xs:mr-0 md:w-6/12 xs:w-full'>
+                            <label htmlFor="Privacy">Prywatność:</label>
+                            <select name="Privacy" onChange={handleInputChange} value={formData.Privacy} required>
+                                <option value="Publiczny">Publiczny</option>
+                                <option value="Prywatny">Prywatny</option>
+                            </select>
+                        </div>
+                        <div className='py-2 md:w-6/12 xs:w-full'>
+                            <label htmlFor="TournamentFormat">Format:</label>
+                            <select name="TournamentFormat" onChange={handleInputChange} value={formData.TournamentFormat} required>
+                                <option value="1v1">1v1</option>
+                                <option value="2v2">2v2</option>
+                                <option value="5v5">5v5</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className='py-2'>
-                        <label htmlFor="GameID">Game:</label>
-                        <select name="GameID" onChange={handleInputChange} value={formData.GameID} required>
-                            <option value="">Wybierz grę</option>
-                            {games.map((game) => (
-                                <option key={game.GameID} value={game.GameID}>
-                                    {game.GameName}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className='py-2'>
-                        <label htmlFor="Privacy">Prywatność:</label>
-                        <select name="Privacy" onChange={handleInputChange} value={formData.Privacy} required>
-                            <option value="Publiczny">Publiczny</option>
-                            <option value="Prywatny">Prywatny</option>
-                        </select>
-                    </div>
-                    <div className='py-2'>
-                        <label htmlFor="TournamentFormat">Format:</label>
-                        <select name="TournamentFormat" onChange={handleInputChange} value={formData.TournamentFormat} required>
-                            <option value="1v1">1v1</option>
-                            <option value="2v2">2v2</option>
-                            <option value="5v5">5v5</option>
-                        </select>
-                    </div>
-                    <div className='py-2'>
-                        <label htmlFor="Prizepool">Prizepool:</label>
-                        <input
-                            type="number"
-                            name="Prizepool"
-                            value={formData.Prizepool}
-                            onChange={handleInputChange}
-                            placeholder="Prizepool"
-                            required
-                        />
-                    </div>
+                        <div className='py-2 md:w-6/12 xs:w-full'>
+                            <label htmlFor="Prizepool">Prizepool:</label>
+                            <input
+                                type="number"
+                                name="Prizepool"
+                                value={formData.Prizepool}
+                                onChange={handleInputChange}
+                                placeholder="Prizepool"
+                                required
+                            />
+                        </div>
+                    
                     <input
                         type="hidden"
                         name="Status"
                         value={formData.Status} // Wartość domyślna, nie wyświetlana użytkownikowi
                     />
-                    <button className='my-2' type="submit">Create Tournament</button>
+                     <button className='my-2' type="submit">Create Tournament</button>
                 </form>
+               
             </div>
         </div>
     );
